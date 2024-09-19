3b27700b92e5d36d24bea89e174eb12812b48f6c
Riddhi Bhagwat <riddhibhagwat@gmail.com>
2024-09-19 12:34:49 -0400

    permissions policy updated

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var currentPath = window.location.pathname;
 
-
+app.use((req, res, next) => {
+    res.setHeader('Permissions-Policy', 'geolocation=(self), microphone=()');
+    next();
+  });
 
 // HOME PAGE
 
